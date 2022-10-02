b145923fd1f1204fc8fd71e27f10960110a3a868
cheneg733 <1170725964@qq.com>
2022-10-02 23:13:43 +0800

    feat: 对工具进行测试

--- src/example/src/index.tsx
+++ src/example/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from "react-dom"
+import { classnames } from '../..'
 
 const App = ()=>{
-    return <div>1221</div>
+    return <div className={classnames('aa')}>1221</div>
 }
 render(<App/>,document.getElementById("#app"))
